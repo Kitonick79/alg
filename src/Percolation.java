944b426c5ff1b1f1f6163051c1ca0e4bcd5114fe
Kitonick79 <dmyagkov@ggasoftware.com>
2016-02-09 18:26:23 +0300

    Initial Commit of Stat. With a bug

--- src/Percolation.java
+++ src/Percolation.java
@@ -5,22 +5,20 @@
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
-    boolean [][] Grid;
-    boolean [][] GridF;
-    WeightedQuickUnionUF GridUF;
-    private int size;
-    private boolean percolates;
-
+    boolean [][] Grid;              // holds status open/bloched of a site. Models percolation Grid
+    WeightedQuickUnionUF GridUFNC;  // To hold the object without top and bottom sites
+    WeightedQuickUnionUF GridUF;    // With bottom and opened sites
+    private int size;               // size of a Grid along axis
 
     public Percolation(int N) {
         size = N;
         Grid = new boolean[size][size];
-        GridUF = new WeightedQuickUnionUF (size*size + 1); // 2 extra nodes above and bellow the matrix
-        GridF = new boolean[size][size];
+        GridUF = new WeightedQuickUnionUF (size*size + 2); // 2 extra nodes above and bellow the matrix
+        GridUFNC = new WeightedQuickUnionUF (size*size + 1); // 2 extra nodes above and bellow the matrix
+
         for (int i=0; i < N; i++){
             for (int j=0; j < N; j++){
                 Grid[i][j] = false; // zero means that site is blocked
-                GridF[i][j] = false; // zero means that site is blocked
             }
         }
     }
@@ -36,64 +34,64 @@ public class Percolation {
     private int convertGridUFIndex(int i, int j) {
         return (i-1)*size + j;
     }
+
     public void open(int i, int j){
-        checkRange(i);
-        checkRange(j);
+        checkRange(i); //index check
+        checkRange(j); //index check
+
         int  i1 = convertGridIndex(i);
         int  j1 = convertGridIndex(j);
+
         int  GridUFPos = convertGridUFIndex(i,j);
 
         Grid[i1][j1] = true; // both indexes starts from zero, one means site is open
 
-        if ( i > 1 && Grid[i1 - 1][j1] ){GridUF.union(GridUFPos, convertGridUFIndex(i - 1,j));} //connect the site above
+        if ( i > 1 && Grid[i1 - 1][j1] ){
+            GridUF.union(GridUFPos, convertGridUFIndex(i - 1,j)); //connect the site above
+            GridUFNC.union(GridUFPos, convertGridUFIndex(i - 1,j));
+        }
 
-        if ( i < size && Grid[i1 +1][j1] ){GridUF.union(GridUFPos, convertGridUFIndex(i + 1, j));} //connect the site bellow
+        if ( i < size && Grid[i1 +1][j1] ){
+            GridUF.union(GridUFPos, convertGridUFIndex(i + 1, j)); //connect the site bellow
+            GridUFNC.union(GridUFPos, convertGridUFIndex(i + 1, j));
 
-        if ( j < size && Grid[i1][j1 + 1] ){GridUF.union(GridUFPos, convertGridUFIndex(i, j + 1));} //connect the site to the right
+        }
 
-        if ( j > 1 && Grid[i1][j1 - 1] ){GridUF.union(GridUFPos, convertGridUFIndex(i, j - 1));} //connect the site to the left
+        if ( j < size && Grid[i1][j1 + 1] ){
+            GridUF.union(GridUFPos, convertGridUFIndex(i, j + 1)); //connect the site to the right
+            GridUFNC.union(GridUFPos, convertGridUFIndex(i, j + 1));
+        }
 
-        if (i == 1){GridUF.union(GridUFPos, 0);}
+        if ( j > 1 && Grid[i1][j1 - 1] ){
+            GridUF.union(GridUFPos, convertGridUFIndex(i, j - 1)); //connect the site to the left
+            GridUFNC.union(GridUFPos, convertGridUFIndex(i, j - 1));
+        } //connect the site to the left
 
+        if (i == 1){
+            GridUF.union(GridUFPos, 0);
+            GridUFNC.union(GridUFPos, 0);}
 
+        if (i == size){GridUF.union(GridUFPos, size*size + 1);} //connect bottom row to last, non-grid node
 
     }
 
     public boolean isOpen(int i, int j){
+        checkRange(i);
+        checkRange(j);
 
         return Grid[convertGridIndex(i)][convertGridIndex(j)];
     }
 
     public boolean isFull(int i, int j) {
+        checkRange(i);
+        checkRange(j);
 
-//               int  i1 = convertGridIndex(i);
-//        int  j1 = convertGridIndex(j);
-//
-//        if (percolates()) {
-//            if (GridF[i1][j1]) {
-//                return true;
-//            } else {
-//                return false;
-//            }
-//        }
-//            else {
-//                if (GridUF.connected(0, convertGridUFIndex(i, j))) {
-//                    GridF[i1][j1] = true;
-//                    return true;
-//                }
-//            }
-//
-        if (i == size && GridUF.connected(0, convertGridUFIndex(i, j))){
-            percolates = true;
-        }
-
-        return GridUF.connected(0, convertGridUFIndex(i, j));
+        return GridUFNC.connected(0, convertGridUFIndex(i, j)); // check if the node at (i, j) is connected to top
     }
 
     public boolean percolates () {
 
-
-        return percolates;
+        return GridUF.connected(0, size*size + 1); // check if top and bottom are connected
     }
 
     public static void main(String[] args) {
--- src/PercolationStats.java
+++ src/PercolationStats.java
@@ -1,57 +1,76 @@
-import edu.princeton.cs.algs4.In;
-import edu.princeton.cs.algs4.StdDraw;
 import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdRandom;
-import java.awt.*;
-import java.util.Arrays;
+import edu.princeton.cs.algs4.StdStats;
 
 /**
- * Created by Admin on 2/6/2016.
+ * Created by Miagkov Dmitrii  on 2/6/2016.
+ *
+ * This is the main class for statistical analysis of percolation of grid,
+ * it takes two parameters:
+ * N - dimension of a Grid
+ * T - number of experiments
+ *
  */
 public class PercolationStats {
-    Percolation perc; // Stores percolation Grid
-    int size;         // Size of the Grid
-    int times;        // Number of experiments
-    int[] openedNumber; // Number of opened sites for each experiment
-    private static final int DELAY = 200;
+    Percolation perc;       // Stores percolation Grid
+    int size;               // Size of the Grid
+    int times;              // Number of experiments
+    double[] openedFrac;    // Fraction of opened sites is each experiment
 
-    public PercolationStats(int N, int T){ // perform T independent experiments on an N-by-N grid
-        size = N;
-        times = T;
-        openedNumber = new int[times];
-        boolean test = false;
+    public PercolationStats(int N, int T) { // perform T independent experiments on an N-by-N grid
+        size = N; //initialization of Grid size
+        times = T; // initialization of number of experiments
+        openedFrac = new double[times]; // setting of array's dimension
 
-        for (int i=0; i < times; i++) {
+        if (size <= 0 || times <= 0) throw  new IllegalMonitorStateException("Size and/or number of experiments are less or equal to zero");
+
+        for (int i = 0; i < times; i++) {
 
             perc = new Percolation(size);
 
-            while (!test) { // run experiments until Grid  percolates
-                int RandomI = StdRandom.uniform(1, N + 1);
-                int RandomJ = StdRandom.uniform(1, N + 1);
+            while (!perc.percolates()) { // run experiments until Grid  percolates
+                int RandomI = StdRandom.uniform(1, N+1);
+                int RandomJ = StdRandom.uniform(1, N+1);
                 if (!perc.isOpen(RandomI, RandomJ) && !perc.isFull(RandomI, RandomJ)) {
                     perc.open(RandomI, RandomJ); // open site chosen at Random
-                    //PercolationVisualizer.draw(perc, N);
-                    //StdDraw.show(DELAY);
-                    openedNumber[i] += 1;  //increase number of opened sites
-                    test = perc.percolates();
+                    openedFrac[i] += 1/((double)size*(double) size);  //increase number of opened sites
                 }
             }
 
         }
     }
 
-    public static void main(String[] args) {
-        //In in = new In(args[0]);      // input file
+    public double mean() { // sample mean of percolation threshold
+
+        return StdStats.mean(openedFrac);
+    }
+
+    public double stddev() {// sample standard deviation of percolation threshold
+
+        return StdStats.stddev(openedFrac);
+    }
+
+    public double confidenceLo() {// low  endpoint of 95% confidence interval
+
+        return (mean()-(1.96 * Math.sqrt(stddev())) / Math.sqrt(times));
+    }
+
+    public double confidenceHi() {// high endpoint of 95% confidence interval
+
+        return (mean()+(1.96 * Math.sqrt(stddev())) / Math.sqrt(times));
+    }
 
 
-        int N = 2;
-        int T = 1;
-////        while (!StdIn.hasNext()) {
-//            N = StdIn.readInt();
-//            T = StdIn.readInt();
-////        }
+    public static void main(String[] args) { // test client
+        int N = StdIn.readInt();
+        int T = StdIn.readInt();
 
         PercolationStats PercStats = new PercolationStats(N, T);
-        System.out.println(Arrays.toString(PercStats.openedNumber));
+       // System.out.println(Arrays.toString(PercStats.openedNumber));
+        System.out.println("mean = " + Double.toString(PercStats.mean()));
+        System.out.println("stddev = " + Double.toString(PercStats.stddev()));
+        System.out.println("95% confidence interval = " + Double.toString(PercStats.confidenceLo()) + ", " + Double.toString(PercStats.confidenceHi()) );
+
+
     }
 }
